c2e4895be38b7d514984e9789c89695ece773231
Oanaa28 <oanamihaeladima1234@gmail.com>
2024-10-17 10:46:39 +0300

    Second Commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 /** * This is a simple Java program.*/
 
 
-class HelloWorld {
+public class HelloWorld {
     /**
      * Your program begins with a call to main().
      * Prints "Hello, World" to the terminal window.
@@ -9,5 +9,40 @@ class HelloWorld {
     public static void main(String args[])
     {
         System.out.println("Hello, World!");
+        boolean yes = true;
+        boolean no = false;
+        int loVal = -999;
+        int hiVal = 999;
+        double grade = 87.5;
+        double amount = 50.0;
+        String hello = "world";
+        System.out.println(yes == no || grade > amount);
+        System.out.println(amount == 40.0 || amount == 50.0);
+        System.out.println(hiVal != loVal || loVal < 0);
+        System.out.println(true || hello.length() > 0);
+        System.out.println(hello.isEmpty() && yes);
+        System.out.println(grade <= 100 && !false);
+        System.out.println(!yes || no);
+        System.out.println(grade > 75 && amount < 75);
+        System.out.println(amount <= hiVal && amount >= loVal);
+        System.out.println(no && !no  || yes && !yes);
+
+//        float a = 100.25f;
+//        long b = (long) a;
+//        System.out.println("value of a: " + a);
+//        System.out.println("value of b: " + b);
+//        int c = (int) b;
+//        System.out.println("value of c: " + c);
+//        byte d = (byte) c;
+//        System.out.println("value of d: " + d);
+
+        int a = 5;
+        System.out.println(a + -a - a++ % 10);
+        System.out.println(a - a + --a /10);
+        for (int i = 2; i < 5;) {
+            i++;
+            a += a;
+        }
+        System.out.println("a = " + a);
     }
 }
